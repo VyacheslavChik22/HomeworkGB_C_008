40d967620062d8914d592a682aacd788ac099fab
Vyacheslav_Zyablov <asyofka@mail.ru>
2023-09-14 20:15:18 +0300

    task_60 done

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -1,7 +1,78 @@
-﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+
+//step1
+int InputNumbers(string phrase)
+{
+    Console.Write(phrase);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+//step2
+int[,,] CreateArray(int length, int height, int width)
+{
+    int[,,] array3D = new int[length, height, width];
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                array3D[i, j, k] = temp[count];
+                count++;
+            }
+        }
+    }
+    return array3D;
+}
+
+//step3
+void PrintArray(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k})" + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+//step4
+int length = InputNumbers("Введите длину массива: ");
+int height = InputNumbers("Введите высоту массива: ");
+int width = InputNumbers("Введите ширину массива: ");
+int[,,] myArray3D = CreateArray(length, height, width);
+PrintArray(myArray3D);
